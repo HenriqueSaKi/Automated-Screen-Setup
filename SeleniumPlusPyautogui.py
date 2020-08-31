bf7e34ccc5a1a26e48425fd752dd2ca2aafce52e
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-08-31 07:33:43 -0300

    Bugs fixed

--- SeleniumPlusPyautogui.py
+++ SeleniumPlusPyautogui.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Created on Mon Jun 22 22:00:41 2020
 @author: Henrique S. Kisaki
@@ -20,40 +21,48 @@ import datetime
 import os
 from datetime import timedelta
 
-options = webdriver.ChromeOptions()
-options.add_argument('lang=pt-br')
-driver = webdriver.Chrome(ChromeDriverManager().install())
-action = ActionChains(driver)
-
 class Telas_Monitoracao():
     #Initialize class
     def __init__ (self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('lang=pt-br')
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.wa = WindowsAction()
         self.datas = [{"instituicao":"Hospital São Luiz São Caetano do Sul", "modelo":"EN - Consumo Energia Elétrica EE CAG"}, {"instituicao":"Hospital São Luiz Itaim", "modelo":"EN - Eficiencia Energética - Medidor CAG"}, {"instituicao":"Hospital Unimed", "modelo":"EN - Consumo Energético - CAG"},{"instituicao":"Honda", "modelo":"EN - Eficiencia Energética CAG"}, {"instituicao":"Hospital São Luiz Morumbi", "modelo":"EIXAD5 - Relatório EE - CAG"}]
-        
 
     #Set contratoManutencao window
     def contratoManutencao_Tela1 (self):
+        action = ActionChains(self.driver)
+        self.InstallRevolverTabExtension()
         time.sleep(2)
-        driver.get('https://g5.oxyn.com.br/')
-        login = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'user_email')))
-        password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'user_senha')))
-        login.send_keys('') #Type your e-mail between ' '
-        password.send_keys('') #Type your password between ' '
-        button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@type='submit']")))
-        button.click()
+        self.driver.get('https://g5.oxyn.com.br/')
+        time.sleep(2)
+        login = self.driver.find_element_by_id("user_email")
+        time.sleep(2)
+        login.click()
+        login.clear()   
+        login.send_keys('')#Write your e-mail between ' ', like: 'example@example.com'
+        password = self.driver.find_element_by_id("user_senha")
+        time.sleep(2)
+        password.click()
+        password.clear()       
+        password.send_keys('')#Write your password between ' ', like: 'PasswordExample123'
+        time.sleep(2)
+        submitButton = self.driver.find_element_by_xpath("//button[@type='submit']")
+        submitButton.click()
         time.sleep(7)
         pyautogui.press('esc')
         time.sleep(2)
-        resumo = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@id='nav']//li[2]"))) #Wait until detect element
+        #Wait until detect element
+        resumo = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@id='nav']//li[2]")))
         resumo.click()
         time.sleep(3)
-        marcador = driver.find_element_by_class_name("marker-label") 
+        marcador = self.driver.find_element_by_class_name("marker-label") 
         marcador.click() 
         time.sleep(2)
         action.reset_actions()
-        contratoBox = driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Contrato de Manutenção')]")
-        filterBox = driver.find_element_by_id("filter")
+        contratoBox = self.driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Contrato de Manutenção')]")
+        filterBox = self.driver.find_element_by_id("filter")
         action.drag_and_drop(contratoBox, filterBox).perform()
         time.sleep(2)
         self.apply()
@@ -61,80 +70,79 @@ class Telas_Monitoracao():
     def contratoManutencao_Tela2 (self):
         for i in range (2):
             self.callTab(i+1)
-            marcador = driver.find_element_by_class_name("marker-label") 
+            action = ActionChains(self.driver)
+            marcador = self.driver.find_element_by_class_name("marker-label") 
             marcador.click() 
             time.sleep(2)
             action.reset_actions()
-            contratoBox = driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Contrato de Manutenção')]")
-            filterBox = driver.find_element_by_id("filter")
+            contratoBox = self.driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Contrato de Manutenção')]")
+            filterBox = self.driver.find_element_by_id("filter")
             action.drag_and_drop(contratoBox, filterBox).perform()
             time.sleep(2)
             action.reset_actions()
             if i == 0:
-                conexaoBox = driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Conexão')]")
+                conexaoBox = self.driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Conexão')]")
             else:
-                conexaoBox = driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Comunicação')]")
-            filterBox1 = driver.find_element_by_xpath("//ul[@id='filter']//li[1]")
+                conexaoBox = self.driver.find_element_by_xpath("//ul[@id='labels']//li[contains(text(),'Comunicação')]")
+            filterBox1 = self.driver.find_element_by_xpath("//ul[@id='filter']//li[1]")
             action.drag_and_drop(conexaoBox, filterBox1).perform()
             time.sleep(2)
-            closeButton = driver.find_element_by_xpath("//ul[@id='filter']//li[2]//a[@href='#']")
+            closeButton = self.driver.find_element_by_xpath("//ul[@id='filter']//li[2]//a[@href='#']")
             closeButton.click()
             time.sleep(2)
             self.apply()
 
-    #Apply filters
     def apply (self):
-        applyButton = driver.find_element_by_xpath("//div[@class='ui-dialog-buttonset']//button[contains(text(),'Aplicar')]")
+        applyButton = self.driver.find_element_by_xpath("//div[@class='ui-dialog-buttonset']//button[contains(text(),'Aplicar')]")
         applyButton.click()
-        time.sleep(2)
+        time.sleep(3)
 
-    #Generate reports
     def generatorButton (self):
-        generator = driver.find_element_by_xpath("//div[@class='ui-dialog-buttonset']//button[contains(text(),'Gerar')]")
+        generator = self.driver.find_element_by_xpath("//div[@class='ui-dialog-buttonset']//button[contains(text(),'Gerar')]")
         generator.click()
         time.sleep(2)
 
-    #Calc a month before and current date
     def dateCalculator (self):
         currentDate = datetime.datetime.today()
         lastMonthDate = currentDate - timedelta(days=31)
-        #Initial date - a month before
-        initialDateField = driver.find_element_by_id('initial-date')
+        #Initial date
+        initialDateField = self.driver.find_element_by_id('initial-date')
         initialDateField.click()
         initialDateField.clear()
+        time.sleep(1)
         initialDateField.send_keys(lastMonthDate.strftime("%d/%m/%Y")) #Add last month date
         pyautogui.press('enter')
-        #Final date - current date
-        finalDateField = driver.find_element_by_id('final-date')
+        #Final date
+        finalDateField = self.driver.find_element_by_id('final-date')
         finalDateField.click()
         finalDateField.clear()
+        time.sleep(1)
         finalDateField.send_keys(currentDate.strftime("%d/%m/%Y"))
         pyautogui.press('enter')
+        time.sleep(2)
 
     def openReport (self):
-        self.callTab(4)
+        self.callTab(3)
         for i in range (5):
             fields = self.datas[i]
-            select = driver.find_element_by_xpath("//span[@id='select2-sites-container']")
+            select = self.driver.find_element_by_xpath("//span[@id='select2-sites-container']")
             select.click()
             time.sleep(2)
-            insertName = driver.find_element_by_xpath("//input[@class='select2-search__field']")
+            insertName = self.driver.find_element_by_xpath("//input[@class='select2-search__field']")
             insertName.send_keys(fields["instituicao"])
             time.sleep(1)
             pyautogui.press('enter')
             time.sleep(5)
-            report = driver.find_element_by_class_name('report')
+            report = self.driver.find_element_by_class_name('report')
             report.click()
             time.sleep(5)
-            reportModel = driver.find_element_by_id('select2-report-model-select2-container')
+            reportModel = self.driver.find_element_by_id('select2-report-model-select2-container')
             reportModel.click()
-            fieldModel = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.CLASS_NAME, 'select2-search__field')))
-            falseModel = driver.find_element_by_xpath("//li[contains(text(), 'Nenhum resultado encontrado')]")
-            while falseModel.is_displayed():
-                fieldModel.click()
-                time.sleep(1)
-            fieldModel.send_keys(fields["modelo"])
+            findModel = WebDriverWait(self.driver, 120).until(EC.presence_of_element_located((By.CLASS_NAME, 'select2-search__field')))
+            time.sleep(5)
+            findModel.send_keys(fields["modelo"])
             pyautogui.press('enter')
+            time.sleep(1)
             self.dateCalculator()
             self.generatorButton()
             self.pageDown()
@@ -143,23 +151,20 @@ class Telas_Monitoracao():
         time.sleep(2)
 
 
-    #Create a new tab with oxyn g5 logged
     def callTab (self, i):
-        driver.execute_script("window.open('');")
-        driver.switch_to.window(driver.window_handles[i])
-        driver.get("https://g5.oxyn.com.br/deviation")
+        self.driver.execute_script("window.open('');")
+        self.driver.switch_to.window(self.driver.window_handles[i])
+        self.driver.get("https://g5.oxyn.com.br/deviation")
         time.sleep(3)
 
-    #Return to report page
     def pageDown (self):
-        driver.switch_to.window(driver.window_handles[3])
+        self.driver.switch_to.window(self.driver.window_handles[3])
         time.sleep(2)
         pyautogui.press('esc')
         time.sleep(1)
 
-    #Recenter content if it isn't in middle screen
     def centerPositionScreen (self):
-        driver.maximize_window()
+        self.driver.maximize_window()
         time.sleep(2)
         pyautogui.moveTo(x=-960, y=15) #Set cursor in first screen
         pyautogui.dragTo(x=960, y=10, duration=0.75) #Drag window to second
@@ -169,28 +174,22 @@ class Telas_Monitoracao():
         pyautogui.click(x=960, y=10)
         time.sleep(2)
 
-
-class InstallRevolverTabExtension:
-    #Access "Report Tabs" page
     def GoogleAccess (self):
-        driver.get('https://chrome.google.com/webstore/detail/revolver-tabs/dlknooajieciikpedpldejhhijacnbda')
+        self.driver.get('https://chrome.google.com/webstore/detail/revolver-tabs/dlknooajieciikpedpldejhhijacnbda?hl=pt-BR')
     
-    #Click in "Usar no Chrome" button 
     def AddExtension (self):
-        addButton = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,"//div[@class='g-c-x']//div[contains(text(),'Usar no Chrome')]")))
+        #resumo = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@id='nav']//li[2]")))
+        addButton = WebDriverWait(self.driver,20).until(EC.presence_of_element_located((By.XPATH,"//div[@class='g-c-x']//div[contains(text(),'Usar no Chrome')]")))
         addButton.click()
     
-    #Access alert box and accept requisition
-    def AcceptRevolverTabExtension (self):
+    def InstallRevolverTabExtension (self):
+        self.GoogleAccess()
+        time.sleep(3)
+        self.AddExtension()
         time.sleep(5)
         pyautogui.press('left')
         pyautogui.press('enter')
-
-    def run(self):
-        self.GoogleAccess()
-        self.AddExtension()
-        self.AcceptRevolverTabExtension()
-
+        time.sleep(5)
 
 class WindowsAction ():
     def __init__(self):
@@ -203,7 +202,7 @@ class WindowsAction ():
         time.sleep(2)
 
     def sendAllToThird (self):
-        pyautogui.click(x=1712, y=15) #Set cursor first report tab
+        pyautogui.click(x=1754, y=15) #Set cursor first report tab
         pyautogui.keyDown('win')
         for i in range (4):
             pyautogui.press('right')
@@ -218,25 +217,25 @@ class WindowsAction ():
             pyautogui.dragTo(x=767, y=15, duration=0.75) #drag to center of second window
             time.sleep(2)
 
-    #find element and click
     def maximizePDF (self):
-        pyautogui.moveTo(x=1838, y=820, duration=2.0)
+        pyautogui.moveTo(x=1853, y=870, duration=2.0)
+        time.sleep(1)
         pyautogui.click()
         time.sleep(1.5)
 
     def reportPosition (self):
-        tab = ['7', '6', '5', '4']
+        tab = ['8', '7', '6', '5', '4']
         clear = lambda: os.system("cls")
         wait = 0
         clear()
-        while wait < 10:
+        while wait < 30:
             clear()
-            print("Wait 10 seconds")
+            print("Wait 30 seconds")
             wait = wait + 1
             time.sleep(0.8)
         print("Ready...")
 
-        for i in range (4):
+        for i in range (5):
             pyautogui.moveTo(x=960, y=540) #center of a 1920x1080 screen
             pyautogui.hotkey('ctrl', tab[i])
             time.sleep(2)
@@ -253,7 +252,6 @@ class WindowsAction ():
 
 tm = Telas_Monitoracao()
 wa = WindowsAction()
-IR = InstallRevolverTabExtension()
 tm.centerPositionScreen()
 tm.contratoManutencao_Tela1()
 tm.contratoManutencao_Tela2()
@@ -262,6 +260,5 @@ wa.reportPosition()
 wa.sendToFirstScreen()
 wa.sendAllToThird()
 wa.secondScreen()
-IR.run()
-pyautogui.click(x=1799, y=64)
-pyautogui.click(x=3719, y=60)
+pyautogui.click(x=1822, y=53)
+pyautogui.click(x=3354, y=53)
